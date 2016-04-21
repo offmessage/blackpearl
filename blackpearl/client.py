7a81af4b167baabec7255bc1f8dada7f3a450090
offmessage <github@offmessage.com>
2016-04-21 09:31:58 +0100

    Handle None output from the change methods (otherwise so much noise)

--- blackpearl/client.py
+++ blackpearl/client.py
@@ -90,84 +90,85 @@ class FlotillaClient(LineReceiver):
             # We should probably emit an 'e' at this point?
             return
         j = self.modules[channel].change(data)
+        if j is None:
+            return
         d = json.loads(j)
-        if d is not None:
-            for s in self.subscribers:
-                s.update(channel, module, data)
+        for s in self.subscribers:
             # here we loop through all the subscribers with the new data
-            print(d)
-            if 'touch' in d:
-                # It's a touch
-                matrix = self.firstOf('matrix')
-                rainbow = self.firstOf('rainbow')
-                motor = self.firstOf('motor')
-                number = self.firstOf('number')
-                if d['touch']['buttons']['1'] and matrix is not None:
-                    if rainbow is not None:
-                        rainbow.reset()
-                        rainbow.set_all(255,255,255)
-                        rainbow.update()
-                    if matrix is not None:
-                        matrix.reset()
-                        matrix.addColumn(127)
-                        matrix.addColumn(1)
-                        matrix.addColumn(3)
-                        matrix.addColumn(7)
-                        matrix.addColumn(15)
-                        matrix.addColumn(31)
-                        matrix.addColumn(63)
-                        matrix.addColumn(127)
-                        matrix.addFrame([255,255,255,255,255,255,255,255])
-                        matrix.addText("1234567890")
-                        matrix.scrollspeed = 2
-                        matrix.loop = True
-                        matrix.frames()
-                if d['touch']['buttons']['2']:
-                    if rainbow is not None:
-                        rainbow.reset()
-                    if matrix is not None:
-                        matrix.reset()
-                    if motor is not None:
-                        motor.stop()
-                    if number is not None:
-                        number.reset()
-                if d['touch']['buttons']['3']:
-                    if rainbow is not None:
-                        rainbow.set_all(255, 0, 0)
-                        rainbow.update()
-                    if number is not None:
-                        number.set_number(1223.2345)
-                        number.update()
-                if d['touch']['buttons']['4']:
-                    if matrix is not None:
-                        matrix.pause()
-            if 'slider' in d:
-                matrix = self.firstOf('matrix')
-                if matrix is not None:
-                    value = d['slider']['value']
-                    if 0 <= value < 200:
-                        speed = 0.3
-                    elif 200 <= value < 400:
-                        speed = 0.2
-                    elif 400 <= value < 600:
-                        speed = 0.1
-                    elif 600 <= value < 800:
-                        speed = 0.07
-                    else:
-                        speed = 0.03
-                    matrix.scrollspeed = speed
-                rainbow = self.firstOf('rainbow')
+            s.update(channel, module, j)
+        print(d)
+        if 'touch' in d:
+            # It's a touch
+            matrix = self.firstOf('matrix')
+            rainbow = self.firstOf('rainbow')
+            motor = self.firstOf('motor')
+            number = self.firstOf('number')
+            if d['touch']['buttons']['1'] and matrix is not None:
                 if rainbow is not None:
-                    color = rainbow.hue(value/1000.0)
-                    rainbow.set_all(*color)
+                    rainbow.reset()
+                    rainbow.set_all(255,255,255)
                     rainbow.update()
-                motor = self.firstOf('motor')
+                if matrix is not None:
+                    matrix.reset()
+                    matrix.addColumn(127)
+                    matrix.addColumn(1)
+                    matrix.addColumn(3)
+                    matrix.addColumn(7)
+                    matrix.addColumn(15)
+                    matrix.addColumn(31)
+                    matrix.addColumn(63)
+                    matrix.addColumn(127)
+                    matrix.addFrame([255,255,255,255,255,255,255,255])
+                    matrix.addText("1234567890")
+                    matrix.scrollspeed = 2
+                    matrix.loop = True
+                    matrix.frames()
+            if d['touch']['buttons']['2']:
+                if rainbow is not None:
+                    rainbow.reset()
+                if matrix is not None:
+                    matrix.reset()
                 if motor is not None:
-                    motor.linearinput(value)
-                number = self.firstOf('number')
+                    motor.stop()
                 if number is not None:
-                    number.set_number(value, pad="0")
+                    number.reset()
+            if d['touch']['buttons']['3']:
+                if rainbow is not None:
+                    rainbow.set_all(255, 0, 0)
+                    rainbow.update()
+                if number is not None:
+                    number.set_number(1223.2345)
                     number.update()
+            if d['touch']['buttons']['4']:
+                if matrix is not None:
+                    matrix.pause()
+        if 'slider' in d:
+            matrix = self.firstOf('matrix')
+            if matrix is not None:
+                value = d['slider']['value']
+                if 0 <= value < 200:
+                    speed = 0.3
+                elif 200 <= value < 400:
+                    speed = 0.2
+                elif 400 <= value < 600:
+                    speed = 0.1
+                elif 600 <= value < 800:
+                    speed = 0.07
+                else:
+                    speed = 0.03
+                matrix.scrollspeed = speed
+            rainbow = self.firstOf('rainbow')
+            if rainbow is not None:
+                color = rainbow.hue(value/1000.0)
+                rainbow.set_all(*color)
+                rainbow.update()
+            motor = self.firstOf('motor')
+            if motor is not None:
+                motor.linearinput(value)
+            number = self.firstOf('number')
+            if number is not None:
+                number.set_number(value, pad="0")
+                number.update()
                 
     def connectedModules(self, type_=None):
         if type_ is None:
