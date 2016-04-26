7a7d01b29b57c0adb045bce05b5f347d6a08cb2c
offmessage <github@offmessage.com>
2016-04-26 10:58:54 +0100

    Imports

--- blackpearl/modules/__init__.py
+++ blackpearl/modules/__init__.py
@@ -10,3 +10,14 @@ input and create output. These are the things that users should be subclasing
 for their projects.
 """
 
+from .colour import Colour
+from .dial import Dial
+from .joystick import Joystick
+from .matrix import Matrix
+from .motion import Motion
+from .motor import Motor
+from .number import Number
+from .rainbow import Rainbow
+from .slider import Slider
+from .touch import Touch
+from .weather import Weather
