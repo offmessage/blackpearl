43cd1021f44d96baa03da69651580b355be55b1d
offmessage <github@offmessage.com>
2016-04-30 14:38:01 +0100

    Annotated code

--- blackpearl/projects/discolights.py
+++ blackpearl/projects/discolights.py
@@ -7,9 +7,15 @@ class DiscoLights(BaseModule):
     hardware_required = ['slider', 'rainbow',]
     
     def data(self, data):
+        # We're listening for the slider, which gives us a value of between
+        # 1 and 1000. We want to turn that into a value between 0 and 1, 
+        # because that's what rainbow.hue() wants
         value = (data['slider']['value'])/1000.0
+        # rainbow.hue() converts a value between 0 and 1 into r, g, b values
         red, green, blue = self.rainbow.hue(value)
+        # Set all the lights on the rainbow to these values
         self.rainbow.set_all(red, green, blue)
+        # Tell the rainbow to use those new values
         self.rainbow.update()
 
 
