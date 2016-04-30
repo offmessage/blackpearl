5dd32825131862061c8333f0706bbbfb5d0ab18c
offmessage <github@offmessage.com>
2016-04-30 13:36:35 +0100

    Added a new project, and annotated the mover.py code better

--- blackpearl/projects/mover.py
+++ blackpearl/projects/mover.py
@@ -7,12 +7,18 @@ class Mover(BaseModule):
     hardware_required = ['slider', 'motor', 'motor',]
     
     def data(self, data):
-        value = data['slider']['value']
+        value = data['slider']['value'] # This is between 0 and 1000
         if value == 0:
+            # if value is 0 then we want maximum reverse speed!
             v = -63
         elif value == 1000:
+            # if value is 1000 then we want maximum forward speed!
             v = 63
         else:
+            # otherwise take 500 off value (so now value is between -500 and +500
+            # and then divide by 8 (so now it's between -63 and 63) and finally
+            # put it through int() so that we knock off the decimal points (by
+            # making it an integer)
             v = int((value - 500)/8)
         
         self.motor1.set_speed(v)
---
+++ blackpearl/projects/stopwatch.py
@@ -0,0 +1,37 @@
+from blackpearl.projects import BaseProject
+
+from blackpearl.modules import BaseModule
+from blackpearl.modules import Timer
+from blackpearl.modules import Touch
+
+
+class Stopwatch(Timer):
+    
+    tick = 0.01
+    hardware_required = ['number', 'touch']
+    listening_for = ['timer', 'touch']
+    
+    def data(self, data):
+        if 'timer' in data:
+            tm = data['timer']['time']
+            self.number.set_number(tm)
+            self.number.update()
+        elif 'touch' in data:
+            buttons = data['touch']['buttons']
+            if buttons['1']:
+                if self.status == 'RUNNING':
+                    self.stop()
+                elif self.status == 'STOPPED':
+                    self.start()
+            if buttons['2'] and self.status == 'STOPPED':
+                self.reset()
+                self.number.set_number(0)
+                self.number.update()
+        
+        
+class Project(BaseProject):
+    required_modules = [Stopwatch, ]
+    
+
+if __name__ == '__main__':
+    Project()
