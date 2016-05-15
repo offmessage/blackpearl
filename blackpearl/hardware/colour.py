03d593fe944d37fc7c58a8c24c05030f73088850
offmessage <github@offmessage.com>
2016-05-15 17:08:34 +0100

    Working on using the real data from the colour module

--- blackpearl/hardware/colour.py
+++ blackpearl/hardware/colour.py
@@ -5,27 +5,68 @@ class ColourInput(FlotillaInput):
     Colour
     ======
     
-    NB: The Flotilla crew ignore the 'brightness' element (c), so I do too
+    https://gadgetoid.gitbooks.io/flotilla-protocol/content/colour.html
     
     Outputs
     -------
     Emits a list in traditional RGB form (e.g ``[r, g, b,]``) where all values
-    are between 0 and 255
+    are between 0 and 255 for rgb
     
-    Example:
+    Example::
     
-    ``{'colour': {'rgb': [255,255,255,]}}``
+      {'colour': {'rgb': [82,72,157,],
+                  'raw': {'red': 441,
+                          'green': 385,
+                          'blue': 834,
+                          'clear': 1361,
+                          }
+                  }
     """
     module = "colour"
-    RGB = [0,0,0,]
+    rgb = [0,0,0,]
+    red = None
+    green = None
+    blue = None
+    clear = None
     
     def change(self, data):
-        r, g, b, c = data.split(b',')
-        rgb = [int(r), int(g), int(b),]
-        if self.RGB == rgb:
-            # This could happen, due to 'c' changing
+        red, green, blue, clear = data.split(b',')
+        red = int(red)
+        green = int(green)
+        blue = int(blue)
+        clear = int(clear)
+        def convert(v):
+            # The fact that we have to put this min() in there is a bit
+            # worrying - I think my personal colour sensor may be over reporting
+            # the levels of blue, as it's often *greater* than the value
+            # reported from the unfiltered light sensor
+            
+            ratio = v/clear
+            value = int(ratio * 255)
+            if value > 255:
+                print("Something went awry - was given a filtered light level"
+                      "greater than the unfiltered light level")
+                return 255
+            return value
+        rgb = [convert(red), convert(green), convert(blue),]
+        
+        print(rgb)
+        
+        if self.rgb == rgb:
+            # This should never happen
             return None
-        self.RGB = rgb
-        return self.broadcast({'rgb': rgb})
+        self.rgb = rgb
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.clear = clear
+        output = {'rgb': rgb,
+                  'raw': {'red': red,
+                          'green': green,
+                          'blue': blue,
+                          'clear': clear,
+                          }
+                  }
+        return self.broadcast(output)
     
     
