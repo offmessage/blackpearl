82d623694ae834b7d17a51d54ae09839a0c649bb
offmessage <github@offmessage.com>
2016-05-29 19:31:00 +0100

    Fixed the light module so that its message actually matched its documentation

--- blackpearl/things/hardware/inputs/light.py
+++ blackpearl/things/hardware/inputs/light.py
@@ -33,11 +33,19 @@ class Light(FlotillaInput):
     
     def change(self, data):
         visible, infrared, lux = data.split(b',')
-        value = int(value)
-        if self.VALUE == value:
-            # This might happen due to dunno1 and dunno2 potentially changing
+        visible = int(visible)
+        infrared = int(infrared)
+        lux = int(lux)
+        if self.visible == visible and self.infrared == infrared and self.lux == lux:
+            # This should never happen
             return None
-        self.VALUE = value
-        return self.broadcast({'level': value,})
+        self.visible = visible
+        self.infrared = infrared
+        self.lux = lux
+        data = {'visible': visible,
+                'infrared': infrared,
+                'lux': lux,
+                }
+        return self.broadcast(data)
 
 
